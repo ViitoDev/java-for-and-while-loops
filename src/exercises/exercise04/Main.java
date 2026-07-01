4572eb136fcdd951e47374a61081796e24f485ac
João Barros <joaovictorpiresdev@gmail.com>
2026-07-01 08:14:12 -0300

    feat: adding the exercise 04

---
+++ src/exercises/exercise04/Main.java
@@ -0,0 +1,17 @@
+package exercises.exercise04;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int number = input.nextInt();
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++){
+            factorial *= i;
+        }
+        System.out.println("The factorial of " + number + " is: " + factorial);
+    }
+}
